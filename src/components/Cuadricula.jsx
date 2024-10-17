5531c0841133cba78097dcc3b802b283cf320227
Gherex <germilagger@gmail.com>
2024-10-17 05:09:50 -0300

    Añadi velocidad a la vibora a medida que pase el tiempo

--- src/components/Cuadricula.jsx
+++ src/components/Cuadricula.jsx
@@ -1,36 +1,28 @@
 import { useRef, useState, useEffect } from "react";
+
 const ARR_CUADRICULA = new Array(576).fill(null);
 const ARR_BORDE_SUPERIOR = Array.from({ length: 24 }, (_, index) => index); // de 0 a 23
-const ARR_BORDE_INFERIOR = Array.from(
-  { length: 24 },
-  (_, index) => index + 552
-); // de 552 a 575
-const ARR_BORDE_IZQUIERDO = Array.from(
-  { length: 24 },
-  (_, index) => index * 24
-); // de 0 a 552 con incremento de 24
-const ARR_BORDE_DERECHO = Array.from(
-  { length: 24 },
-  (_, index) => 23 + index * 24
-); // de 23 a 575 con incremento de 24
+const ARR_BORDE_INFERIOR = Array.from({ length: 24 }, (_, index) => index + 552); // de 552 a 575
+const ARR_BORDE_IZQUIERDO = Array.from({ length: 24 }, (_, index) => index * 24); // de 0 a 552 con incremento de 24
+const ARR_BORDE_DERECHO = Array.from({ length: 24 }, (_, index) => 23 + index * 24); // de 23 a 575 con incremento de 24
 
 function Cuadricula({ onPlay, onGameOver }) {
-  let [posicionesTablero, setPosicionesTablero] = useState(
+  const [posicionesTablero, setPosicionesTablero] = useState(
     new Array(576).fill(false)
   );
-  let [arraySnake, setArraySnake] = useState([250, 251, 252]);
-  let [posCabeza, setPosCabeza] = useState(252);
-  let [direccion, setDireccion] = useState("right"); // posibles valores: left | right | up | down
-  let [posComida, setPosComida] = useState(null); // Inicializa la comida como null
-  const divRef = useRef(null); // Referencia al div
-  let [iniciarJuego, setIniciarJuego] = useState(false);
-  let [endGame, setEndGame] = useState(false);
-  let [puntos, setPuntos] = useState(0);
+  const [arraySnake, setArraySnake] = useState([250, 251, 252]);
+  const [posCabeza, setPosCabeza] = useState(252);
+  const [direccion, setDireccion] = useState("right");
+  const [posComida, setPosComida] = useState(null);
+  const divRef = useRef(null);
+  const [iniciarJuego, setIniciarJuego] = useState(false);
+  const [endGame, setEndGame] = useState(false);
+  const [puntos, setPuntos] = useState(0);
+  const [velVibora, setVelVibora] = useState(400);
 
-  /* Posicion inicial de la vibora */
   useEffect(() => {
     if (divRef.current) {
-      divRef.current.focus(); // Focaliza el div al montarse
+      divRef.current.focus();
     }
     return () => {
       setPosicionesTablero(new Array(576).fill(false));
@@ -40,108 +32,123 @@ function Cuadricula({ onPlay, onGameOver }) {
   }, []);
 
   useEffect(() => {
-    let newPosTablero = new Array(576).fill(false);
+    const newPosTablero = new Array(576).fill(false);
     arraySnake.forEach((pos) => {
-      if (pos <= newPosTablero.length) {
+      if (pos < newPosTablero.length) {
         newPosTablero[pos] = true;
       }
     });
     setPosicionesTablero(newPosTablero);
   }, [arraySnake]);
 
-  // efecto para generar la posición aleatoria de la comida
   useEffect(() => {
-    function generarPosicionAleatoria() {
+    if (posComida === null) {
       let pos;
       do {
         pos = Math.floor(Math.random() * 576);
-      } while (arraySnake.includes(pos)); // para que la comida no aparezca en la vibora
+      } while (arraySnake.includes(pos));
       setPosComida(pos);
-    }
-
-    // Generar comida al inicio del juego
-    if (posComida === null) {
-      generarPosicionAleatoria();
     }
   }, [posComida, arraySnake]);
 
   function handleKeyDown(event) {
-    if (event.key === "w" || event.key === "W" || event.key === "ArrowUp")
-      if (direccion !== "up" && direccion !== "down") setDireccion("up");
-    if (event.key === "a" || event.key === "A" || event.key === "ArrowLeft")
-      if (direccion !== "left" && direccion !== "right") setDireccion("left");
-    if (event.key === "s" || event.key === "S" || event.key === "ArrowDown")
-      if (direccion !== "down" && direccion !== "up") setDireccion("down");
-    if (event.key === "d" || event.key === "D" || event.key === "ArrowRight")
-      if (direccion !== "right" && direccion !== "left") setDireccion("right");
+    const keyMap = {
+      w: "up",
+      W: "up",
+      ArrowUp: "up",
+      a: "left",
+      A: "left",
+      ArrowLeft: "left",
+      s: "down",
+      S: "down",
+      ArrowDown: "down",
+      d: "right",
+      D: "right",
+      ArrowRight: "right",
+    };
 
+    const newDirection = keyMap[event.key];
+    if (
+      newDirection &&
+      newDirection !== direccion &&
+      !(
+        (direccion === "up" && newDirection === "down") ||
+        (direccion === "down" && newDirection === "up") ||
+        (direccion === "left" && newDirection === "right") ||
+        (direccion === "right" && newDirection === "left")
+      )
+    ) {
+      setDireccion(newDirection);
+    }
     setIniciarJuego(true);
     onPlay(true);
   }
 
   useEffect(() => {
+    let intervalId = null;
+
     function moverVibora() {
       let newArraySnake = [...arraySnake];
       let newPosCabeza = posCabeza;
 
+      // Lógica de dirección y movimiento
       if (direccion === "right") {
-        if (ARR_BORDE_DERECHO.includes(newPosCabeza)) {
-          newPosCabeza = newPosCabeza - 23;
-        } else {
-          newPosCabeza = newPosCabeza + 1;
-        }
+        newPosCabeza = ARR_BORDE_DERECHO.includes(newPosCabeza)
+          ? newPosCabeza - 23
+          : newPosCabeza + 1;
       } else if (direccion === "left") {
-        if (ARR_BORDE_IZQUIERDO.includes(newPosCabeza)) {
-          newPosCabeza = newPosCabeza + 23;
-        } else {
-          newPosCabeza = newPosCabeza - 1;
-        }
+        newPosCabeza = ARR_BORDE_IZQUIERDO.includes(newPosCabeza)
+          ? newPosCabeza + 23
+          : newPosCabeza - 1;
       } else if (direccion === "up") {
-        if (ARR_BORDE_SUPERIOR.includes(newPosCabeza)) {
-          newPosCabeza = newPosCabeza + 552;
-        } else {
-          newPosCabeza = newPosCabeza - 24;
-        }
+        newPosCabeza = ARR_BORDE_SUPERIOR.includes(newPosCabeza)
+          ? newPosCabeza + 552
+          : newPosCabeza - 24;
       } else if (direccion === "down") {
-        if (ARR_BORDE_INFERIOR.includes(newPosCabeza)) {
-          newPosCabeza = newPosCabeza - 552;
-        } else {
-          newPosCabeza = newPosCabeza + 24;
-        }
+        newPosCabeza = ARR_BORDE_INFERIOR.includes(newPosCabeza)
+          ? newPosCabeza - 552
+          : newPosCabeza + 24;
       }
 
-      // Detectar si la vibora toca la comida
+      // Detecta si la víbora toca la comida
       if (newPosCabeza === posComida) {
-        setPosComida(null); // Al tocar la comida, reiniciamos su posición
-        setPuntos((puntos) => puntos + 10);
+        setPosComida(null);
+        setPuntos(puntos + 10);
       } else {
-        newArraySnake.shift();
+        newArraySnake.shift(); // Quita la cola si no ha comido
       }
 
-      // Detectamos si se choca consigo misma
+      // Detecta colisiones con el cuerpo
       if (newArraySnake.includes(newPosCabeza)) {
         onGameOver(true);
         setEndGame(true);
+        clearInterval(intervalId);
         return;
       }
 
       newArraySnake.push(newPosCabeza);
       setArraySnake(newArraySnake);
       setPosCabeza(newPosCabeza);
+      setVelVibora((prevVel) => (prevVel > 50 ? prevVel - 1 : prevVel));
     }
 
     if (iniciarJuego) {
-      const intervalID = setInterval(() => {
-        moverVibora();
-      }, 200);
+      intervalId = setInterval(moverVibora, velVibora);
 
-      return () => clearInterval(intervalID);
+      return () => {
+        clearInterval(intervalId);
+      };
     }
-  }, [iniciarJuego, direccion, posCabeza, arraySnake, posComida, onGameOver]);
-
-  function snakeIsHere(posicion) {
-    return posicionesTablero[posicion];
-  }
+  }, [
+    iniciarJuego,
+    direccion,
+    posCabeza,
+    arraySnake,
+    posComida,
+    onGameOver,
+    velVibora,
+    puntos,
+  ]);
 
   function handleMenuClick() {
     setPosicionesTablero(new Array(576).fill(false));
@@ -183,7 +190,7 @@ function Cuadricula({ onPlay, onGameOver }) {
               className="bg-red-900 border-2 border-black w-2.5 h-2.5 sm:w-3 sm:h-3 md:w-4 md:h-4"
             ></div>
           );
-        } else if (snakeIsHere(index)) {
+        } else if (posicionesTablero[index]) {
           return (
             <div
               key={index}
